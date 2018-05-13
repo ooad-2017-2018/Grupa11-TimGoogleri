e42e2031a8004a013e7d37ae1a4d4498a372eb52
ntarakcija <ntarakcija1@etf.unsa.ba>
2018-05-13 22:52:04 +0200

    Update

---
+++ Projekat/ProjekatETFManager/ProjekatETFManager/Models/Osoba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatETFManager.Models
+{
+    class Osoba
+    {
+        private String username { get; set; }
+        private String password { get; set; }
+
+        public bool passwordCheck(String pw) {
+            return true;
+        }
+
+    }
+}
---
+++ Projekat/ProjekatETFManager/ProjekatETFManager/View Models/SystemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatETFManager.View_Models
+{
+    class SystemViewModel
+    {
+        public SystemViewModel() { }
+        public void upravljanjeBazon() { }
+        public void updateIgraca() { }
+        public void dodavanjeKorisnika() { }
+        public void brisanjeKorisnika() { }
+        public void dodavanjeLige() { }
+        public void brisanjeLige() { }
+        public void dodavanjeTima() { }
+        public void brisanjeTima() { }
+
+    }
+}
